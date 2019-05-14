26cea5e3bc83a394be3e986f1cfda53783e1c226
chema <chema22web@gmail.com>
2019-05-14 17:39:25 +0200

    fichero de texto

--- 03_LecturaClientes/src/com/sinensia/MainEjercicio.java
+++ 03_LecturaClientes/src/com/sinensia/MainEjercicio.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.sinensia;
 
 import java.io.File;
@@ -10,17 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
+
 public class MainEjercicio {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        File fichRutaRelativa = new File("../Ejercicio_ficheros.txt");
+        File fichRutaRelativa = new File("Ejercicio_ficheros.txt");
         Scanner escaner = null;
         HashMap<Long, Cliente> mapaDat = new HashMap<>();
         try {

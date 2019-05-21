ae76bbb5200df84418b2b78b6b0a3a22eac1c1ca
chema <chema22web@gmail.com>
2019-05-21 09:10:43 +0200

    jdbc meter datos en bd

--- 06_WebServlets/src/java/com/sinensia/RegistroServlet.java
+++ 06_WebServlets/src/java/com/sinensia/RegistroServlet.java
@@ -1,6 +1,7 @@
 
 package com.sinensia;
 
+import com.sinensia.modelo.MySQLCliente;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -26,12 +27,50 @@ public class RegistroServlet extends HttpServlet {
            salida.println("<title>Web servlets desde java</title>");
            salida.println("</head><body>");
            salida.println("<h1> Respuesta al registro </h1>");
+           salida.println("<h2>ruta del servlet: "+peticion.getContextPath()+" metodo: "+peticion.getMethod()+"</h2>");
+           salida.println("<ul>");
+           
+           for(int i =0;i<3;i++){
+               salida.println("<li> Numm: " +i+ " </li>");
+           }
+           salida.println("</ul>");
+           salida.println("<p>"+respuesta.toString() +"</p>"); // org.netbeans.modules.web.monitor.server.MonitorResponseWrapper@7b34897e
+           salida.println("<p>"+respuesta.getContentType() +"</p>"); //text/html;charset=UTF-8
+           
+    //************************* COGEMOS LOS VALORES DEL CLIENTE **********************************
+           String nombre = peticion.getParameter("nombre");
+           String strEdad = peticion.getParameter("edad");
+           //PARSEAMOS EL STRING DE EDAD
+           int edad = Integer.parseInt(strEdad);
+           String email = peticion.getParameter("email");
+           String strActivo = peticion.getParameter("activo");
+           String password = peticion.getParameter("password");
+           //PARSEAMOS EL BOOLEAN DE ACTIVO0.
+           //si el valor es distinto de null, que ponga el valor
+           boolean activo = strActivo != null;
+           salida.println("<p>nombre: "+ nombre+ " </p>");
+           salida.println("<p>edad: "+ edad+ " </p>");
+           salida.println("<p>email: "+ email+ " </p>");
+           salida.println("<p>activo: "+ activo+ " </p>");
+           salida.println("<p>password: "+ password+ " </p>");
+    //********************************************************************************************
+    
+    MySQLCliente bdCliente = new MySQLCliente();
+    
+    
+   if ( bdCliente.crear(nombre, email, password, edad, activo)){
+   
+       salida.println("<p>guardado correctamente</p>");
+   } else {
+        salida.println("<p>no se guardo!!</p>");
+   }
+
            salida.println("</body></html>");
            salida.println("");
            salida.close();
       } 
   }
- 
+                                                                                  
     
   @Override
    public String getServletInfo (){
@@ -40,4 +79,4 @@ public class RegistroServlet extends HttpServlet {
    }
     
 
-}
+}//fin clase registroservlet
---
+++ 06_WebServlets/src/java/com/sinensia/modelo/MySQLCliente.java
@@ -0,0 +1,51 @@
+
+package com.sinensia.modelo;
+
+import static com.mysql.cj.x.protobuf.MysqlxPrepare.Prepare.OneOfMessage.Type.INSERT;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MySQLCliente {
+    
+    public MySQLCliente(){
+    
+        try {
+            //ruta donde esta el driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            //CARGAMOS Y DRIVER Y RECOGEMOS LOS POSIBLES ERRORES
+        }catch(SQLException ex){
+           
+            Logger.getLogger(MySQLCliente.class.getName()).log(Level.SEVERE, "Error en el sql: ", ex);
+        } catch(ClassNotFoundException ex){
+            
+            Logger.getLogger(MySQLCliente.class.getName()).log(Level.SEVERE, "Error en la clase: ", ex);
+        } catch(Exception ex){
+            
+            Logger.getLogger(MySQLCliente.class.getName()).log(Level.SEVERE, "Otro error: ", ex);
+        }
+    }
+    
+    public boolean crear(String nombre, String email, String password, int edad, boolean activo){
+        
+        try(Connection conex = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_ventas?user=admin&password=Admin1234&useSSL=false&serverTimezone=UTC",
+                "admin", "Admin1234")){
+       //CREAMOS LA CONSULTA
+       String sqlQuery = "INSERT INTO cliente (nombre, email, password, edad, activo) VALUES ('" + nombre + "', '"+ email +"', '"+ password +"', "+ edad + ", "
+               +(activo ? "1": "0")+")";
+            Statement sentencia = conex.createStatement();
+            sentencia.executeUpdate(sqlQuery);
+            return true;
+        //
+        } catch(SQLException ex){
+         Logger.getLogger(MySQLCliente.class.getName()).log(Level.SEVERE, "Error en el sql: ", ex);
+         return false;
+
+    }
+}
+
+}
